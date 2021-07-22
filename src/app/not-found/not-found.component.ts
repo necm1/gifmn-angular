e7196f110d624b8f7655db411b2bf5c93a7d2420
necm1 <necmi.m.hasan@outlook.de>
2021-07-22 03:12:42 +0200

    added not found page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,17 +1,38 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
+import {TitleService} from '../_service/title.service';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
+/**
+ * @class NotFoundComponent
+ * @implements {OnInit, OnDestroy}
+ */
 export class NotFoundComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  /**
+   * NotFoundComponent Constructor
+   *
+   * @constructor
+   * @param title
+   * @param translate
+   */
+  constructor(
+    private title: TitleService,
+    private translate: TranslateService
+  ) { }
 
   public ngOnInit(): void {
     document.getElementsByTagName('app-header')[0]?.classList.add('d-none');
+    const subscription = this.translate.get('404.title').subscribe({
+      next: value => this.title.set(value),
+      complete: () => subscription.unsubscribe()
+    });
   }
+
 
   /**
    * @public
